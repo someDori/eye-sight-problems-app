3ee9b87c89606bcaedb7381f0b0f9753eed8f2c7
someDori <demetrepanjakidze1@gmail.com>
2025-01-04 15:27:22 +0400

    Initial commit

---
+++ eyeSightProblems/eyeSightProblemsApp.swift
@@ -0,0 +1,17 @@
+//
+//  eyeSightProblemsApp.swift
+//  eyeSightProblems
+//
+//  Created by Demetre Panjakidze on 04.01.25.
+//
+
+import SwiftUI
+
+@main
+struct eyeSightProblemsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
